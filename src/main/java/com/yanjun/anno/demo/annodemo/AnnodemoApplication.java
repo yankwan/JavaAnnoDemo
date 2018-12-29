f86f8d1f6283febf1bb804b3ebd7d8484e72175d
liuyanjun <yanjliu@126.com>
2018-12-29 17:11:46 +0800

    git init

---
+++ src/main/java/com/yanjun/anno/demo/annodemo/AnnodemoApplication.java
@@ -0,0 +1,14 @@
+package com.yanjun.anno.demo.annodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnodemoApplication.class, args);
+    }
+
+}
+
